9a7f9efa17bf436d558eb53ab32d59d8d954a41c
Frederic Lanic <f.lanic-ext@lefebvre-sarrut.eu>
2021-04-15 20:07:09 +0200

    copyright 2021

--- src/main/java/com/kycox/game/level/CheckScreenBlockBorders.java
+++ src/main/java/com/kycox/game/level/CheckScreenBlockBorders.java
@@ -1,5 +1,5 @@
 /**
-  Copyright (C) 2019  Frédéric Lanic frederic.lanic@outlook.fr
+  Copyright (C) 2021  Frédéric Lanic frederic.lanic@outlook.fr
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
