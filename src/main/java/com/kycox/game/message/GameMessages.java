b3441a0944ac0553baf20a344fd20da38660dc1f
Kikox <frederic.lanic@outlook.fr>
2023-02-07 18:45:43 +0100

    [TECH] nano refacto

--- src/main/java/com/kycox/game/message/GameMessages.java
+++ src/main/java/com/kycox/game/message/GameMessages.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 public enum GameMessages {
 	GAME_IN_PAUSE("game in pause"), HEY_WE_GO("here we go"), I_AM_GHOST("i am ghost"), I_AM_LADYBUG("i am ladybug"),
-	ONE_PLAYER_MODE("single player mode"), SOUND_ACTIVE("sound activated"), SOUND_OFF("sound off"),
-	TWO_PLAYERS_MODE("two player mode"), XBOX_GHOST_CONNECTION("xboxone ghost connected"),
+	ONE_PLAYER_MODE("single player mode"), SOUND_ACTIVE("sound on"), SOUND_OFF("sound off"),
+	TWO_PLAYERS_MODE("two players mode"), XBOX_GHOST_CONNECTION("xboxone ghost connected"),
 	XBOX_GHOST_DISCONNECTION("xboxone ghost disconnected"), XBOX_LADYBUG_CONNECTION("xboxone ladybug connected"),
 	XBOX_LADYBUG_DISCONNECTION("xboxone ladybug disconnected"), INITIALIZE_LEVEL_NUMBER("initialize level number");
 
