f6f18d0bb8a51c58f8b856e1ca8c1f94785f1552
Kikox <frederic.lanic@outlook.fr>
2023-02-25 17:43:26 +0100

    [TECH] Choix du rendu aléatoire V1 lors du jeu

--- src/main/java/com/kycox/game/constant/level/LevelImages.java
+++ src/main/java/com/kycox/game/constant/level/LevelImages.java
@@ -21,10 +21,10 @@ public enum LevelImages {
 
 	NO_RIGHT_LEFT_NO_UP_NO_DOWN("no_right_left_no_up_no_down.png"), NO_RIGHT_NO_LEFT_NO_UP_NO_DOWN("no_right_no_left_no_up_no_down.png"),
 
-	COIN("gold.png"), MEGA_GOLD("megaGold.png");
+	GOLD("gold.png"), MEGA_GOLD("megaGold.png");
 
 	@Getter
-	private Image image;
+	private final Image image;
 
 	LevelImages(String fileName) {
 		var pathName = "images/level/" + fileName;
